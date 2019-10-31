d66f24d2ab62c17c8dab208727c436724512d125
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2019-10-31 18:27:21 +0800

    fix struct tag map

--- database.go
+++ database.go
@@ -37,7 +37,37 @@ func StructTagMap(tag string, model interface{}) (map[string]interface{}, error)
 
 		offset := field.Offset
 		pointer := unsafe.Pointer(firstAddr + offset)
-		tagMap[key] = interface{}(pointer)
+
+		// "tagMap[key] = interface{}(pointer)" will return type error when sql.Scan
+		// need exact type
+		value := elem.FieldByName(field.Name).Interface()
+		switch value.(type) {
+		case string:
+			tagMap[key] = (*string)(pointer)
+
+		case int:
+			tagMap[key] = (*int)(pointer)
+		case int8:
+			tagMap[key] = (*int8)(pointer)
+		case int16:
+			tagMap[key] = (*int16)(pointer)
+		case int32:
+			tagMap[key] = (*int32)(pointer)
+		case int64:
+			tagMap[key] = (*int64)(pointer)
+
+		case float32:
+			tagMap[key] = (*float32)(pointer)
+		case float64:
+			tagMap[key] = (*float64)(pointer)
+		default:
+			return nil, fmt.Errorf(
+				"unsupport %s type name: %s kind: %s",
+				key,
+				field.Type.Name(),
+				field.Type.Kind().String(),
+			)
+		}
 
 		// set value
 		// elem.FieldByName(field.Name).Set(reflect.ValueOf(key))
--- test/main.go
+++ test/main.go
@@ -6,11 +6,6 @@ import (
 	"github.com/m_quadra/kameria"
 )
 
-type testModel struct {
-	KeyName string `dbkey:"key1"`
-	KeyUsr  string `dbkey:"key2"`
-}
-
 func main() {
 	fmt.Println("kameria の test")
 
@@ -20,20 +15,33 @@ func main() {
 	ipv4Str, err = kameria.IPv4IntToString(ipv4Num)
 	fmt.Println(ipv4Num, "->", ipv4Str, err)
 
-	var model testModel
-	model.KeyName = "name1"
-	model.KeyUsr = "usr"
+	structTagMapTest()
+}
 
+func structTagMapTest() {
+	fmt.Println("\nStructTagMap(tag string, model interface{}) (map[string]interface{}, error)")
+
+	type tsModel struct {
+		KeyName string `dbkey:"key1"`
+		KeyUser string `dbkey:"key2"`
+	}
+
+	var model tsModel
 	needMap := map[string]interface{}{
 		"key1": &model.KeyName,
-		"key2": &model.KeyUsr,
+		"key2": &model.KeyUser,
 	}
-	fmt.Println("      needMap:", needMap)
+	fmt.Println("needMap:", needMap)
 
 	tagMap, err := kameria.StructTagMap("dbkey", &model)
 	if kameria.HasError(err) {
 		fmt.Println(".StructTagMap error")
 	} else {
-		fmt.Println(".StructTagMap:", tagMap)
+		fmt.Println(" tagMap:", tagMap)
+	}
+	for k, v := range tagMap {
+		if needMap[k] != v {
+			fmt.Println("???", k, "fail")
+		}
 	}
 }
