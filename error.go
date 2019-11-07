73a3879552feb8d0b1fdaba6079e3ffc266154ae
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2019-11-07 18:27:36 +0800

    update error printer

--- error.go
+++ error.go
@@ -1,8 +1,10 @@
 package kameria
 
-import "fmt"
-
-var lastErrStr string
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
 // HasError error printer
 func HasError(err error) bool {
@@ -10,10 +12,13 @@ func HasError(err error) bool {
 		return false
 	}
 
-	errStr := err.Error()
-	if errStr != lastErrStr {
-		fmt.Println(errStr)
-		lastErrStr = errStr
+	ptr, file, line, ok := runtime.Caller(1)
+	if ok {
+		nowTime := time.Now()
+		fmt.Println("error happen:")
+		fmt.Println("    ", nowTime.Format("2006-01-02 15:04:05"), nowTime.Unix(), nowTime.UnixNano())
+		fmt.Printf("     %s:%d +%#x\n", file, line, ptr)
+		fmt.Println("    ", err)
 	}
 	return true
 }
