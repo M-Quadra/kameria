dc1f02e364a8470071fad4bb63a0dda07e6029ee
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2019-11-14 18:25:57 +0800

    update cmp

--- math.go
+++ math.go
@@ -1,5 +1,7 @@
 package kameria
 
+import "math"
+
 //Max4Int return max ∈ [min, max]
 func Max4Int(a, b int) int {
 	if a < b {
@@ -16,6 +18,11 @@ func Max4Int64(a, b int64) int64 {
 	return a
 }
 
+//Max4Float32 return max ∈ [min, max]
+func Max4Float32(a, b float32) float32 {
+	return float32(math.Max(float64(a), float64(b)))
+}
+
 //Min4Int return min ∈ [min, max]
 func Min4Int(a, b int) int {
 	if a < b {
@@ -30,6 +37,11 @@ func Min4Int64(a, b int64) int64 {
 		return a
 	}
 	return b
+}
+
+//Min4Float32 return min ∈ [min, max]
+func Min4Float32(a, b float32) float32 {
+	return float32(math.Min(float64(a), float64(b)))
 }
 
 //Limit4Int return mid ∈ [min, max]
@@ -51,5 +63,20 @@ func Limit4Int64(min, mid, max int64) int64 {
 
 	mid = Max4Int64(min, mid)
 	mid = Min4Int64(mid, max)
+	return mid
+}
+
+//Limit4Float32 return mid ∈ [min, max]
+func Limit4Float32(min, mid, max float32) float32 {
+	return float32(Limit4Float64(float64(min), float64(mid), float64(max)))
+}
+
+//Limit4Float64 return mid ∈ [min, max]
+func Limit4Float64(min, mid, max float64) float64 {
+	nMin := math.Min(min, max)
+	nMax := math.Max(min, max)
+
+	mid = math.Max(nMin, mid)
+	mid = math.Min(mid, nMax)
 	return mid
 }
