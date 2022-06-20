4225925b1fcb5ea9ecba135bf38653e317a9cadd
M_noAria <46508354+M-Quadra@users.noreply.github.com>
2022-06-20 21:30:44 +0800

    remove reflect of Reverse

--- algorithm/reverse.go
+++ algorithm/reverse.go
@@ -1,17 +1,12 @@
 package algorithm
 
-import (
-	"reflect"
-)
-
 // Reverse std::reverse
 func Reverse[T any](slice []T, first, last int) {
 	if len(slice) <= 0 {
 		return
 	}
 
-	swap := reflect.Swapper(slice)
 	for last--; first < last; first, last = first+1, last-1 {
-		swap(first, last)
+		slice[first], slice[last] = slice[last], slice[first]
 	}
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.18
 
 require (
 	github.com/M-Quadra/kazaana v1.1.1
-	github.com/gin-gonic/gin v1.8.0
-	github.com/stretchr/testify v1.7.1
-	golang.org/x/exp v0.0.0-20220518171630-0b5c67f07fdf
+	github.com/gin-gonic/gin v1.8.1
+	github.com/stretchr/testify v1.7.2
+	golang.org/x/exp v0.0.0-20220613132600-b0d781184e0d
 )
 
 require (
